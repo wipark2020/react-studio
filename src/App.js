b6cd802517416ec0f747cd2cf933953aca80c230
wipark2020 <78107368+wipark2020@users.noreply.github.com>
2024-03-14 17:40:50 -0400

    bro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
@@ -12,17 +12,71 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  // make a state variable to hold the data
+  const [data, setData] = useState(bakeryData);
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  // run when this component is first loaded
+  const loadData = () => {
+    setData(bakeryData);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
+  useEffect(() => {
+    let total = 0;
+    for (let item of cart) {
+      total += item.price;
+    }
+    setCartTotal(total);
+  }, [cart]);
+
+  const addToCart = (item) => {
+    console.log('adding to cart:', item.name, item.price)
+
+    setCart(prev_cart =>
+      [...prev_cart, {name: item.name, price: item.price}])
+  }
+
+  const buildElements = () => {
+    const jsxlist = bakeryData.map((item, index) => (
+      <div key={index} onClick={() => addToCart(item)} className="BakeryItem">
+        <img src={item.image} alt={item.name}/>
+        <p><b>{item.name}</b></p>
+        <p>{item.description}</p>
+        <p>${item.price}</p>
+      </div>
+    ));
+    
+    return jsxlist;
+  }
+
+  const showCart = () => {
+    if (cart.length === 0) {
+      console.log('cart is empty')
+      return <p>Cart is empty</p>
+    }
+
+    const jsxlist = cart.map((item, index) => {
+      return <p key={index} > {item.name} $ {item.price}</p>
+    })
+
+    return jsxlist;
+  }
+
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+      <h1>Will's Bakery</h1> 
+      <div className="BakeryItems">
+        {buildElements()}
+      </div>
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {showCart()}
+        <p><b>Total: ${cartTotal.toFixed(2)}</b></p>
       </div>
     </div>
   );
